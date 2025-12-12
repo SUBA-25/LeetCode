26afad008c4789e6ad43624c5699ccba9d5a63b6
SUBASHINI P <subamanju25@gmail.com>
2025-12-12 11:09:17 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.83%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,21 +1,51 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        
-        String pref = strs[0];
-        int prefLen = pref.length();
-
-        for (int i = 1; i < strs.length; i++) {
-            String s = strs[i];
-            while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
-                prefLen--;
-                if (prefLen == 0) {
-                    return "";
-                }
-                pref = pref.substring(0, prefLen);
+        String pre=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(!strs[i].startsWith(pre)){
+                pre=pre.substring(0,pre.length()-1);
+                if(pre.length()==0) return "";
             }
         }
+        return pre;
+        // if (strs == null || strs.length == 0) return "";
+        
+        // String pref = strs[0];
+        // int prefLen = pref.length();
 
-        return pref;        
+        // for (int i = 1; i < strs.length; i++) {
+        //     String s = strs[i];
+        //     while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
+        //         prefLen--;
+        //         if (prefLen == 0) {
+        //             return "";
+        //         }
+        //         pref = pref.substring(0, prefLen);
+        //     }
+        // }
+
+        // return pref;        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
